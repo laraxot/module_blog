b30986aaab4da8feafb277a77cd6baddcc711bcf
marco76tv <marco.sottana@gmail.com>
2022-06-07 11:26:32 +0200

    .

---
+++ Database/Factories/ImageFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+use Modules\Blog\Models\Image;
+
+class ImageFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Image::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition() {
+       
+
+        return [
+            'id' => $this->faker->randomNumber(),
+            'post_type' => $this->faker->word,
+            'post_id' => $this->faker->randomNumber(),
+            'src' => $this->faker->word,
+            'note' => $this->faker->word
+        ];
+    }
+}
