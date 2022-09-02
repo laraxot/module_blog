dfffb547698ae9d96c714d94f509cd57f174532b
marco76tv <marco.sottana@gmail.com>
2022-09-02 11:49:49 +0200

    up

---
+++ Database/Migrations/2020_04_27_200108_create_labels_table.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+//----- bases ----
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+/**
+ * Class CreateLabelsTable.
+ */
+class CreateLabelsTable extends XotBaseMigration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        //-- CREATE --
+        $this->tableCreate(
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->nullableMorphs('label');
+                $table->string('title')->nullable();
+                $table->string('created_by')->nullable();
+                $table->string('updated_by')->nullable();
+                $table->timestamps();
+           }
+            );
+
+
+        //-- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table) {
+            if (! $this->hasColumn('class')) {
+                $table->string('class')->nullable();
+            }
+        });
+    }
+
+    //end up
+}//end class
---
+++ Models/Panels/PhotoPanel.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Models\Panels;
+
+//--- Services --
+use Modules\Rating\Models\Panels\Actions\RateItAction;
+use Modules\Xot\Models\Panels\XotBasePanel;
+
+//---- bases --
+
+/**
+ * Class PhotoPanel.
+ */
+class PhotoPanel extends XotBasePanel {
+    /**
+     * The model the resource corresponds to.
+     */
+    protected static string $model = 'Modules\Blog\Models\Photo';
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     */
+    protected static string $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     */
+    protected static array $search = [
+    ];
+
+    /**
+     * The relationships that should be eager loaded on index queries.
+     */
+    public function with(): array {
+        return [];
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     */
+    public function fields(): array {
+        return [
+            (object) [
+                'type' => 'Id',
+                'name' => 'id',
+                'comment' => null,
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'post.title',
+                'col_size' => 12,
+                //'except' => ['index'],
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'post.image_title',
+                'col_size' => 12,
+                //'except' => ['index'],
+            ],
+
+            (object) [
+                'type' => 'Text',
+                'name' => 'post.image_alt',
+                'col_size' => 12,
+                //'except' => ['index'],
+            ],
+
+            (object) [
+                //'type' => 'UnisharpImg', //'Html5UploadImg',
+                'type' => 'Image',
+                'name' => 'post.image_src',
+                'col_size' => 12,
+                //'except' => ['index'],
+            ],
+        ];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     */
+    public function actions(): array {
+        return [
+            new RateItAction(),
+            //new \Modules\Xot\Models\Panels\Actions\UploadImageTestAction(),
+        ];
+    }
+}
