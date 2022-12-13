d1ce7ae2d73d10b79a2acec771062c0db08fdb3a
marco xot <marco.sottana@gmail.com>
2022-12-13 16:41:53 +0100

    .

--- docs/navigation.php
+++ docs/navigation.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'Getting Started' => [
         'url' => 'docs/getting-started',
@@ -8,6 +10,7 @@ return [
             'Navigation' => 'docs/navigation',
             'Algolia DocSearch' => 'docs/algolia-docsearch',
             'Custom 404 Page' => 'docs/custom-404-page',
+            'Test' => 'docs/test',
         ],
     ],
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
