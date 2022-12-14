e777d470b42ed476934911adac9f5ade8b081528
marco76tv <marco76tv@users.noreply.github.com>
2022-12-14 13:07:55 +0000

    Lint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,3 +1,5 @@
 <?php
 
-return include getcwd()."/vendor/orchestra/testbench-core/laravel/bootstrap/app.php";
+declare(strict_types=1);
+
+return include getcwd().'/vendor/orchestra/testbench-core/laravel/bootstrap/app.php';
