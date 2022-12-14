e605d1597a3c33d7e78a042b7c2e6fc2c3966f38
marco xot <marco.sottana@gmail.com>
2022-12-14 09:24:50 +0100

    .

--- docs/navigation.php
+++ docs/navigation.php
@@ -6,7 +6,7 @@ return [
     'Getting Started' => [
         'url' => 'docs/getting-started',
         'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
+            'Modelli' => 'docs/modelli',
             'Navigation' => 'docs/navigation',
             'Algolia DocSearch' => 'docs/algolia-docsearch',
             'Custom 404 Page' => 'docs/custom-404-page',
