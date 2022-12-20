e95c07b178d04cc342541ae872f069fceadfe5f7
marco76tv <marco.sottana@gmail.com>
2022-12-20 09:13:37 +0100

    up

---
+++ Tests/Unit/ArticleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Tests\Unit;
+
+use Modules\Blog\Tests\TestCase;
+
+// use Tests\TestCase;
+// use Orchestra\Testbench\TestCase;
+
+class ArticleTest extends TestCase {
+    public function testExample(): void {
+        $this->assertTrue(true);
+    }
+}
