797adbe8a71c63c3779a733c8f5bf4817abd28b0
marco xot <marco.sottana@gmail.com>
2022-12-27 14:23:45 +0100

    .

--- Models/Panels/PagePanel.php
+++ Models/Panels/PagePanel.php
@@ -6,7 +6,6 @@ namespace Modules\Blog\Models\Panels;
 
 // --- Services --
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Modules\Blog\Models\Page;
 use Modules\Blog\Models\Panels\Traits\XotBasePanelTrait;
@@ -17,7 +16,8 @@ use Modules\Cms\Models\Panels\XotBasePanel;
 /**
  * Class PagePanel.
  */
-class PagePanel extends XotBasePanel {
+class PagePanel extends XotBasePanel
+{
     use XotBasePanelTrait;
     /**
      * The model the resource corresponds to.
@@ -32,15 +32,15 @@ class PagePanel extends XotBasePanel {
     /**
      * The columns that should be searched.
      */
-    protected static array $search = [
-    ];
+    protected static array $search = [];
 
     /**
      * The relationships that should be eager loaded on index queries.
      *
      * @return string[]
      */
-    public function with(): array {
+    public function with(): array
+    {
         return ['post'];
     }
 
@@ -55,22 +55,25 @@ class PagePanel extends XotBasePanel {
      *
      * @param Page $row
      */
-    public function optionLabel($row): string {
+    public function optionLabel($row): string
+    {
         return (string) $row->title;
     }
 
     /**
      * index navigation.
      */
-    public function indexNav(): ?Renderable {
-        [$containers,$items] = params2ContainerItem();
+    public function indexNav(): ?Renderable
+    {
+        dddx('qui');
+        [$containers, $items] = params2ContainerItem();
         $last_item = last($items);
-        if (! inAdmin()) {
+        if (!inAdmin()) {
             // siccome non so dove metterlo, per ora dentro pub_theme
             /**
              * @phpstan-var view-string
              */
-            $view = 'pub_theme::pages.'.$last_item.'.index.nav';
+            $view = 'pub_theme::pages.' . $last_item . '.index.nav';
 
             return view()->make($view);
         }
@@ -81,7 +84,9 @@ class PagePanel extends XotBasePanel {
     /**
      * Get the fields displayed by the resource.
      */
-    public function fields(): array {
+    public function fields(): array
+    {
+
         return [
             (object) [
                 'type' => 'Id',
@@ -174,20 +179,22 @@ class PagePanel extends XotBasePanel {
     /**
      * Get the actions available for the resource.
      */
-    public function actions(): array {
+    public function actions(): array
+    {
         return [
             new Actions\SendMsgAction(),
         ];
     }
 
     // temporaneo perchè altrimenti mi da /it/pages/0
-    public function url(string $act = 'show', array $params = []): string {
+    public function url(string $act = 'show', array $params = []): string
+    {
         $url = $this->route->{__FUNCTION__}($act);
 
         if ([] !== $params) {
             $url_components = parse_url($url);
-            if (! isset($url_components['path'])) {
-                throw new \Exception('['.__LINE__.']['.__FILE__.']');
+            if (!isset($url_components['path'])) {
+                throw new \Exception('[' . __LINE__ . '][' . __FILE__ . ']');
             }
             $url = $url_components['path'];
 
@@ -197,7 +204,7 @@ class PagePanel extends XotBasePanel {
                 $merged = array_replace_recursive($originalParams, $params);
             }
 
-            $url .= '?'.Arr::query($merged);
+            $url .= '?' . Arr::query($merged);
         }
 
         return $url;
