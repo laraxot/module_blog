378fec2204d8d30bd1dab4b6faf4109893440719
marco76tv <marco.sottana@gmail.com>
2023-01-17 10:58:54 +0100

    up

--- Models/Contact.php
+++ Models/Contact.php
@@ -7,44 +7,25 @@ namespace Modules\Blog\Models;
 /**
  * Modules\Blog\Models\Contact.
  *
- * @property string|null                                                          $guid
- * @property string|null                                                          $image_src
- * @property string|null                                                          $lang
- * @property string|null                                                          $post_type
- * @property string|null                                                          $subtitle
- * @property string|null                                                          $title
- * @property string|null                                                          $txt
- * @property string|null                                                          $user_handle
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Image[] $images
- * @property int|null                                                             $images_count
- * @property \Modules\Lang\Models\Post|null                                       $post
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
- * @property int|null                                                             $posts_count
- * @property mixed                                                                $url
- *
- * @method static \Modules\Blog\Database\Factories\ContactFactory     factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact       newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Contact       newQuery()
+ * @property string|null $guid
+ * @property string|null $image_src
+ * @property-read string|null $lang
+ * @property-read string|null $post_type
+ * @property string|null $subtitle
+ * @property string|null $title
+ * @property string|null $txt
+ * @property-read string|null $user_handle
+ * @property-read \Modules\Lang\Models\Post|null $post
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
+ * @property-read int|null $posts_count
+ * @property-write mixed $url
+ * @method static \Modules\Blog\Database\Factories\ContactFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact       query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
- *
  * @mixin \Eloquent
- *
- * @property int                             $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property string|null                     $created_by
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null                     $updated_by
- * @property int|null                        $post_id
- *
- * @method static \Illuminate\Database\Eloquent\Builder|Contact whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact wherePostId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact wherePostType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact whereUpdatedBy($value)
  */
 class Contact extends BaseModelLang {
     protected $fillable = ['id', 'post_id', 'post_type'];
