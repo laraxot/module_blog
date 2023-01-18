0b422f6f68944c42fc7d80757a18cbcd71e879b3
Malebestia <malebestia@yahoo.it>
2023-01-18 09:41:24 +0100

    up

--- Models/ExtraFieldGroup.php
+++ Models/ExtraFieldGroup.php
@@ -6,4 +6,8 @@ namespace Modules\Blog\Models;
 
 class ExtraFieldGroup extends BaseModel {
     protected $fillable = ['id', 'name', 'cardinality'];
+
+    public function extraFields() {
+        return $this->hasMany(ExtraField::class);
+    }
 }
