6c99a657cb5eb1891a5f7675133b116bde0a6d05
marco76tv <marco.sottana@gmail.com>
2023-01-23 19:24:21 +0100

    .

---
+++ Database/Factories/PostFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Module\Blog\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Module\Blog\Tests\TestSupport\Models\Post;
+
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word,
+        ];
+    }
+}
