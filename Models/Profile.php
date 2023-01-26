000210a04fd83c513e9181bb8ffdcb59e3b19eb8
marco76tv <marco.sottana@gmail.com>
2023-01-26 18:23:42 +0100

    rebase

--- Models/Profile.php
+++ Models/Profile.php
@@ -356,6 +356,7 @@ use Spatie\ModelStatus\HasStatuses;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 class Profile extends BaseModelLang
 {
     use GeoTrait;
@@ -370,6 +371,11 @@ class Profile extends BaseModelLang {
 >>>>>>> afa2edf (Lint)
     // use GeoTrait; -- to profile in geo
 >>>>>>> 0cf44f8 (up)
+=======
+class Profile extends BaseModelLang
+{
+    use GeoTrait;
+>>>>>>> 92d6d85 (.)
     use HasProfileTrait;
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -447,7 +453,8 @@ class Profile extends BaseModelLang {
 >>>>>>> 7424c65 (rebase)
     // ------- RELATIONSHIP ----------
 
-    public function articles(): HasMany {
+    public function articles(): HasMany
+    {
         return $this->hasMany(Article::class, 'author_id', 'user_id');
     }
 
@@ -468,7 +475,8 @@ class Profile extends BaseModelLang {
     }
     */
 
-    protected function username(): Attribute {
+    protected function username(): Attribute
+    {
         $user = $this->user;
         if (null == $user) {
             // $user1 = User::firstOrCreate(['id' => $this->user_id]);
@@ -510,7 +518,8 @@ class Profile extends BaseModelLang {
         );
     }
 
-    protected function name(): Attribute {
+    protected function name(): Attribute
+    {
         $user = $this->user;
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -564,13 +573,15 @@ class Profile extends BaseModelLang {
     }
     */
 
-    protected function githubUsername(): Attribute {
+    protected function githubUsername(): Attribute
+    {
         return Attribute::make(
             get: fn ($value) => $this->github_username ?? '',
         );
     }
 
-    protected function hasTwitterAccount(): Attribute {
+    protected function hasTwitterAccount(): Attribute
+    {
         return Attribute::make(
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -600,17 +611,20 @@ class Profile extends BaseModelLang {
         );
     }
 
-    protected function twitter(): Attribute {
+    protected function twitter(): Attribute
+    {
         return Attribute::make(
             get: fn ($value) => '',
         );
     }
 
-    public function isLoggedInUser(): bool {
+    public function isLoggedInUser(): bool
+    {
         return Auth::id() === $this->getKey();
     }
 
-    public function isBanned(): bool {
+    public function isBanned(): bool
+    {
         return 'ban' === $this->status;
     }
 }// end model
