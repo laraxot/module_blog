5b1cc63301354afb486c49161b61efb88b6c74d2
marco76tv <marco.sottana@gmail.com>
2023-01-31 17:53:17 +0100

    rebase

--- Models/Panels/Policies/CategorizablePanelPolicy.php
+++ Models/Panels/Policies/CategorizablePanelPolicy.php
@@ -1,9 +1,18 @@
 <?php
+<<<<<<< HEAD
 
 declare(strict_types=1);
 
 namespace Modules\Blog\Models\Panels\Policies;
 
+=======
+namespace Modules\Blog\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Blog\Models\Panels\Policies\CategorizablePanelPolicy as Post; 
+
+>>>>>>> 4011d2d (up)
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
 class CategorizablePanelPolicy extends XotBasePanelPolicy {
