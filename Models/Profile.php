025051303d9e0084adc18a2c6e040823a72acf02
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:30:56 +0100

    rebase

--- Models/Profile.php
+++ Models/Profile.php
@@ -7,6 +7,7 @@ namespace Modules\Blog\Models;
 // --------- models --------
 
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
+<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 <<<<<<< HEAD
@@ -18,7 +19,16 @@ use Modules\Blog\Models\Traits\PrivacyTrait;
 use Modules\Geo\Models\Traits\GeoTrait;
 use Modules\LU\Casts\UserField;
 use Modules\LU\Models\Traits\HasProfileTrait;
+=======
+>>>>>>> 6b1fa56 (.)
 use Modules\LU\Models\User;
+use Modules\LU\Casts\UserField;
+use Spatie\ModelStatus\HasStatuses;
+use Illuminate\Support\Facades\Auth;
+use Modules\Geo\Models\Traits\GeoTrait;
+use Modules\Xot\Models\Traits\WidgetTrait;
+use Modules\Blog\Models\Traits\PrivacyTrait;
+use Modules\LU\Models\Traits\HasProfileTrait;
 use Modules\Ticket\Models\Traits\HasTicketTrait;
 use Modules\Xot\Models\Traits\WidgetTrait;
 use Spatie\ModelStatus\HasStatuses;
@@ -107,6 +117,7 @@ use Spatie\ModelStatus\HasStatuses;
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
  *
  * @method static \Illuminate\Database\Eloquent\Builder|Profile       currentStatus(...$names)
 =======
@@ -124,6 +135,8 @@ use Spatie\ModelStatus\HasStatuses;
 =======
  *
 >>>>>>> ee04bc7 (.)
+=======
+>>>>>>> 6b1fa56 (.)
  * @method static \Modules\Blog\Database\Factories\ProfileFactory     factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile       newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Profile       newQuery()
@@ -177,9 +190,7 @@ use Spatie\ModelStatus\HasStatuses;
  * @method static \Illuminate\Database\Eloquent\Builder|Profile       whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile       whereUserId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
- *
  * @mixin \Eloquent
- *
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereFirstName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Profile whereLastName($value)
  *
