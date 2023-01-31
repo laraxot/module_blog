cd887066782d249b0286875dda1d641f31e8d191
marco76tv <marco.sottana@gmail.com>
2023-01-31 13:57:58 +0100

    .

--- Resources/lang/nl/notifications.php
+++ Resources/lang/nl/notifications.php
@@ -1,46 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 declare(strict_types=1);
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-declare(strict_types=1);
-
-=======
->>>>>>> 6c99a65 (.)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e7b73dd (rebase)
-=======
-declare(strict_types=1);
-
->>>>>>> 5c25f35 (Lint)
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-=======
-declare(strict_types=1);
-
->>>>>>> 6d76482 (up)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
 return [
     'approve_comment' => 'Keur opmerking goed',
     'reject_comment' => 'Keur opmerking af',
@@ -53,44 +14,7 @@ return [
     'approved_comment_mail_title' => 'Een nieuwe opmerking op ":commentable_name"',
     'approved_comment_mail_body' => 'Geschreven door :commentator_name',
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     'enum_description_participating' => 'Bij deelname aan gesprek',
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-    'enum_description_participating' => 'Bij deelname aan gesprek',
-=======
-    'enum_description_participating'=> 'Bij deelname aan gesprek',
->>>>>>> 6c99a65 (.)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    'enum_description_participating' => 'Bij deelname aan gesprek',
->>>>>>> 5c25f35 (Lint)
-=======
->>>>>>> f98d93c (rebase)
-=======
-=======
-    'enum_description_participating' => 'Bij deelname aan gesprek',
->>>>>>> 5c25f35 (Lint)
->>>>>>> e7b73dd (rebase)
-=======
-    'enum_description_participating' => 'Bij deelname aan gesprek',
->>>>>>> 6d76482 (up)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
     'enum_description_all' => 'Bij alle opmerkingen',
     'enum_description_none' => 'Nooit',
     'enum_longdescription_participating' => 'Krijg een notificatie op dit gesprek',
