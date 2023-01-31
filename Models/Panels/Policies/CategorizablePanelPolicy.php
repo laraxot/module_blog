aee319d34cc3b2f862d874c815b724bca5bda683
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:40:08 +0100

    rebase

--- Models/Panels/Policies/CategorizablePanelPolicy.php
+++ Models/Panels/Policies/CategorizablePanelPolicy.php
@@ -1,6 +1,9 @@
 <?php
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> fedd096 (rebase)
 
 declare(strict_types=1);
 
@@ -8,12 +11,19 @@ namespace Modules\Blog\Models\Panels\Policies;
 
 =======
 namespace Modules\Blog\Models\Panels\Policies;
+<<<<<<< HEAD
 =======
 >>>>>>> 89651d8 (Lint)
 
 declare(strict_types=1);
 
 namespace Modules\Blog\Models\Panels\Policies;
+=======
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Blog\Models\Panels\Policies\CategorizablePanelPolicy as Post; 
+>>>>>>> fedd096 (rebase)
 
 >>>>>>> 8009709 (up)
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
