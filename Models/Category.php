20ea9212ca57bc3a19f6ac41767e8a9768b76ffc
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:29:55 +0100

    rebase

--- Models/Category.php
+++ Models/Category.php
@@ -218,10 +218,14 @@ class Category extends Model {
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     public function scopeOfType(Builder $query, string $type): Builder {
 =======
     public function scopeOfType(Builder $query, string $type):Builder {
 >>>>>>> 7fdf4e1 (up)
+=======
+    public function scopeOfType(Builder $query, string $type): Builder {
+>>>>>>> 134e178 (Lint)
         return $query->whereRelation('categorizables', 'categorizable_type', $type);
         /*
         return $query->whereHas('categorizables',function($q) use($type){
