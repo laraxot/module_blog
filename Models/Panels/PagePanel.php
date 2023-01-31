e73a4b0efd9abbba257f4c4db8cb6a2eb5db6448
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:32:49 +0100

    rebase

--- Models/Panels/PagePanel.php
+++ Models/Panels/PagePanel.php
@@ -51,8 +51,7 @@ use Modules\Cms\Models\Panels\XotBasePanel;
 /**
  * Class PagePanel.
  */
-class PagePanel extends XotBasePanel
-{
+class PagePanel extends XotBasePanel {
     use XotBasePanelTrait;
     /**
      * The model the resource corresponds to.
@@ -74,8 +73,7 @@ class PagePanel extends XotBasePanel
      *
      * @return string[]
      */
-    public function with(): array
-    {
+    public function with(): array {
         return ['post'];
     }
 
@@ -90,8 +88,7 @@ class PagePanel extends XotBasePanel
      *
      * @param Page $row
      */
-    public function optionLabel($row): string
-    {
+    public function optionLabel($row): string {
         return (string) $row->title;
     }
 
@@ -101,8 +98,11 @@ class PagePanel extends XotBasePanel
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> c505cad (rebase)
+=======
+>>>>>>> 3063b59 (rebase)
     public function indexNav(): ?Renderable {
 =======
     public function indexNav(): ?Renderable
@@ -114,16 +114,22 @@ class PagePanel extends XotBasePanel
 >>>>>>> f980f97 (Lint)
 =======
 >>>>>>> c029664 (.)
+<<<<<<< HEAD
 >>>>>>> c505cad (rebase)
+=======
+=======
+    public function indexNav(): ?Renderable {
+>>>>>>> 9a30267 (Lint)
+>>>>>>> 3063b59 (rebase)
         dddx('qui');
         [$containers, $items] = params2ContainerItem();
         $last_item = last($items);
-        if (!inAdmin()) {
+        if (! inAdmin()) {
             // siccome non so dove metterlo, per ora dentro pub_theme
             /**
              * @phpstan-var view-string
              */
-            $view = 'pub_theme::pages.' . $last_item . '.index.nav';
+            $view = 'pub_theme::pages.'.$last_item.'.index.nav';
 
             return view()->make($view);
         }
@@ -134,9 +140,7 @@ class PagePanel extends XotBasePanel
     /**
      * Get the fields displayed by the resource.
      */
-    public function fields(): array
-    {
-
+    public function fields(): array {
         return [
             (object) [
                 'type' => 'Id',
@@ -229,20 +233,19 @@ class PagePanel extends XotBasePanel
     /**
      * Get the actions available for the resource.
      */
-    public function actions(): array
-    {
+    public function actions(): array {
         return [
             new Actions\SendMsgAction(),
         ];
     }
 
     // temporaneo perchè altrimenti mi da /it/pages/0
-    public function url(string $act = 'show', array $params = []): string
-    {
+    public function url(string $act = 'show', array $params = []): string {
         $url = $this->route->{__FUNCTION__}($act);
 
         if ([] !== $params) {
             $url_components = parse_url($url);
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -256,6 +259,8 @@ class PagePanel extends XotBasePanel
 >>>>>>> af0dac7 (rebase)
 =======
 >>>>>>> c505cad (rebase)
+=======
+>>>>>>> 3063b59 (rebase)
             if (! isset($url_components['path'])) {
                 throw new \Exception('['.__LINE__.']['.__FILE__.']');
 =======
@@ -291,7 +296,14 @@ class PagePanel extends XotBasePanel
             if (!isset($url_components['path'])) {
                 throw new \Exception('[' . __LINE__ . '][' . __FILE__ . ']');
 >>>>>>> c029664 (.)
+<<<<<<< HEAD
 >>>>>>> c505cad (rebase)
+=======
+=======
+            if (! isset($url_components['path'])) {
+                throw new \Exception('['.__LINE__.']['.__FILE__.']');
+>>>>>>> 9a30267 (Lint)
+>>>>>>> 3063b59 (rebase)
             }
             $url = $url_components['path'];
 
@@ -301,7 +313,7 @@ class PagePanel extends XotBasePanel
                 $merged = array_replace_recursive($originalParams, $params);
             }
 
-            $url .= '?' . Arr::query($merged);
+            $url .= '?'.Arr::query($merged);
         }
 
         return $url;
