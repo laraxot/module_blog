b1268df5e480d84a0e7c0ed5a2dab19a9ccec246
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:34:23 +0100

    rebase

--- Models/Category.php
+++ Models/Category.php
@@ -167,8 +167,7 @@ use Spatie\Translatable\HasTranslations;
  *
  * @mixin \Eloquent
  */
-class Category extends Model
-{
+class Category extends Model {
     use HasFactory;
     use HasSlug;
     use HasTranslations;
@@ -253,8 +252,7 @@ class Category extends Model
     /**
      * Get all attached models of the given class to the category.
      */
-    public function entries(string $class): MorphToMany
-    {
+    public function entries(string $class): MorphToMany {
         // return $this->morphedByMany($class, 'categorizable', config('rinvex.categories.tables.categorizables'), 'category_id', 'categorizable_id', 'id', 'id');
         return $this->morphedByMany($class, 'categorizable', 'categorizable', 'category_id', 'categorizable_id', 'id', 'id');
     }
@@ -262,14 +260,14 @@ class Category extends Model
     /**
      * Get the options for generating the slug.
      */
-    public function getSlugOptions(): SlugOptions
-    {
+    public function getSlugOptions(): SlugOptions {
         return SlugOptions::create()
                           // ->doNotGenerateSlugsOnUpdate() // ?
                           ->generateSlugsFrom('name')
                           ->saveSlugsTo('slug');
     }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -325,6 +323,9 @@ class Category extends Model
     public function scopeOfType(Builder $query, string $type): Builder
     {
 >>>>>>> 92d6d85 (.)
+=======
+    public function scopeOfType(Builder $query, string $type): Builder {
+>>>>>>> 0834824 (Lint)
         return $query->whereRelation('categorizables', 'categorizable_type', $type);
         /*
         return $query->whereHas('categorizables',function($q) use($type){
@@ -333,21 +334,18 @@ class Category extends Model
         */
     }
 
-    public function categorizables(): HasMany
-    {
+    public function categorizables(): HasMany {
         return $this->hasMany(Categorizable::class, 'category_id');
     }
 
-    public function articles(): MorphToMany
-    {
+    public function articles(): MorphToMany {
         return $this->morphedByMany(self::class, 'categorizable', 'categorizable', 'category_id', 'categorizable_id', 'id', 'id');
     }
 
     /**
      * @return string
      */
-    public function getRouteKeyName()
-    {
+    public function getRouteKeyName() {
         return RouteService::inAdmin() ? 'id' : 'slug';
     }
 }
