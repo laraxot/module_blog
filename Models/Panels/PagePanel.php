95ad474e6f131aad1d35bb4c6a70f149b4c72e40
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:32:45 +0100

    rebase

--- Models/Panels/PagePanel.php
+++ Models/Panels/PagePanel.php
@@ -32,9 +32,15 @@ use Exception;
 =======
 =======
 use Illuminate\Contracts\Support\Renderable;
+<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Model;
 >>>>>>> 1daf8a3 (Lint)
+<<<<<<< HEAD
 >>>>>>> af0dac7 (rebase)
+=======
+=======
+>>>>>>> c029664 (.)
+>>>>>>> c505cad (rebase)
 use Illuminate\Support\Arr;
 use Modules\Blog\Models\Page;
 use Modules\Blog\Models\Panels\Traits\XotBasePanelTrait;
@@ -45,7 +51,8 @@ use Modules\Cms\Models\Panels\XotBasePanel;
 /**
  * Class PagePanel.
  */
-class PagePanel extends XotBasePanel {
+class PagePanel extends XotBasePanel
+{
     use XotBasePanelTrait;
     /**
      * The model the resource corresponds to.
@@ -67,7 +74,8 @@ class PagePanel extends XotBasePanel {
      *
      * @return string[]
      */
-    public function with(): array {
+    public function with(): array
+    {
         return ['post'];
     }
 
@@ -82,7 +90,8 @@ class PagePanel extends XotBasePanel {
      *
      * @param Page $row
      */
-    public function optionLabel($row): string {
+    public function optionLabel($row): string
+    {
         return (string) $row->title;
     }
 
@@ -91,23 +100,30 @@ class PagePanel extends XotBasePanel {
      */
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c505cad (rebase)
     public function indexNav(): ?Renderable {
 =======
     public function indexNav(): ?Renderable
     {
+<<<<<<< HEAD
 >>>>>>> 9f910bb (.)
 =======
     public function indexNav(): ?Renderable {
 >>>>>>> f980f97 (Lint)
+=======
+>>>>>>> c029664 (.)
+>>>>>>> c505cad (rebase)
         dddx('qui');
         [$containers, $items] = params2ContainerItem();
         $last_item = last($items);
-        if (! inAdmin()) {
+        if (!inAdmin()) {
             // siccome non so dove metterlo, per ora dentro pub_theme
             /**
              * @phpstan-var view-string
              */
-            $view = 'pub_theme::pages.'.$last_item.'.index.nav';
+            $view = 'pub_theme::pages.' . $last_item . '.index.nav';
 
             return view()->make($view);
         }
@@ -118,7 +134,9 @@ class PagePanel extends XotBasePanel {
     /**
      * Get the fields displayed by the resource.
      */
-    public function fields(): array {
+    public function fields(): array
+    {
+
         return [
             (object) [
                 'type' => 'Id',
@@ -211,14 +229,16 @@ class PagePanel extends XotBasePanel {
     /**
      * Get the actions available for the resource.
      */
-    public function actions(): array {
+    public function actions(): array
+    {
         return [
             new Actions\SendMsgAction(),
         ];
     }
 
     // temporaneo perchè altrimenti mi da /it/pages/0
-    public function url(string $act = 'show', array $params = []): string {
+    public function url(string $act = 'show', array $params = []): string
+    {
         $url = $this->route->{__FUNCTION__}($act);
 
         if ([] !== $params) {
@@ -229,10 +249,13 @@ class PagePanel extends XotBasePanel {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> f930b9f (rebase)
 =======
 >>>>>>> af0dac7 (rebase)
+=======
+>>>>>>> c505cad (rebase)
             if (! isset($url_components['path'])) {
                 throw new \Exception('['.__LINE__.']['.__FILE__.']');
 =======
@@ -261,7 +284,14 @@ class PagePanel extends XotBasePanel {
             if (! isset($url_components['path'])) {
                 throw new \Exception('['.__LINE__.']['.__FILE__.']');
 >>>>>>> 1daf8a3 (Lint)
+<<<<<<< HEAD
 >>>>>>> af0dac7 (rebase)
+=======
+=======
+            if (!isset($url_components['path'])) {
+                throw new \Exception('[' . __LINE__ . '][' . __FILE__ . ']');
+>>>>>>> c029664 (.)
+>>>>>>> c505cad (rebase)
             }
             $url = $url_components['path'];
 
@@ -271,7 +301,7 @@ class PagePanel extends XotBasePanel {
                 $merged = array_replace_recursive($originalParams, $params);
             }
 
-            $url .= '?'.Arr::query($merged);
+            $url .= '?' . Arr::query($merged);
         }
 
         return $url;
