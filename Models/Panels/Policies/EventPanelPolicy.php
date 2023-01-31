fec1b3ce42d6fbd874765d737b52622f75c26a5b
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:28:04 +0100

    rebase

---
+++ Models/Panels/Policies/EventPanelPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Blog\Models\Panels\Policies;
+
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
+
+/*
+use Modules\Xot\Models\Policies\XotBasePolicy;
+se estendo
+Declaration of Modules\Blog\Models\Policies\EventPolicy::index(Modules\LU\Models\User $user, Modules\Blog\Models\Event $post) should be compatible with Modules\Xot\Models\Policies\XotBasePolicy::index(Modules\LU\Models\User $user, $post)
+*/
+
+/**
+ * Class EventPanelPolicy.
+ */
+class EventPanelPolicy extends XotBasePanelPolicy {
+}
--- Models/Panels/Policies/ProfilePanelPolicy.php
+++ Models/Panels/Policies/ProfilePanelPolicy.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace Modules\Blog\Models\Panels\Policies;
 
 use Modules\Cms\Contracts\PanelContract;
+<<<<<<< HEAD
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 use Modules\Xot\Contracts\UserContract;
+=======
+use Modules\Xot\Contracts\UserContract;
+use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
+>>>>>>> 315e874 (up)
 
 /**
  * Class ProfilePanelPolicy.
