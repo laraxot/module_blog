379960b6a6fbdeb0c7a74dea606a7753beee1215
marco76tv <marco.sottana@gmail.com>
2023-01-31 13:51:09 +0100

    .

--- Models/Panels/Actions/PersonalInfoAction.php
+++ Models/Panels/Actions/PersonalInfoAction.php
@@ -10,28 +10,6 @@ namespace Modules\Blog\Models\Panels\Actions;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Support\Facades\Session;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8513cb9 (rebase)
-=======
-use Modules\Theme\Services\ThemeService;
->>>>>>> 5a5000f (up)
-=======
->>>>>>> a91a603 (Lint)
-<<<<<<< HEAD
-=======
-=======
-use Modules\Theme\Services\ThemeService;
->>>>>>> 5a5000f (up)
->>>>>>> 658d838 (rebase)
-=======
->>>>>>> 8513cb9 (rebase)
-=======
->>>>>>> 6d76482 (up)
 use Modules\Cms\Models\Panels\Actions\XotBasePanelAction;
 
 // -------- bases -----------
@@ -46,129 +24,8 @@ class PersonalInfoAction extends XotBasePanelAction {
 
     public string $icon = '<i class="far fa-file-excel fa-1x"></i>';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 47d8285 (rebase)
-=======
->>>>>>> 355a234 (rebase)
-=======
->>>>>>> ea4d650 (rebase)
-=======
->>>>>>> ae00439 (rebase)
-    public function handle(): Renderable {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 0088a11 (rebase)
-=======
->>>>>>> aff4d2d (rebase)
-    public function handle(): Renderable
-    {
->>>>>>> 00a7996 (.)
-=======
-    public function handle(): Renderable
-    {
-=======
-    public function handle(): Renderable {
->>>>>>> afa2edf (Lint)
->>>>>>> f0ffa9e (rebase)
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> 0088a11 (rebase)
-    public function handle(): Renderable
-    {
->>>>>>> 00a7996 (.)
->>>>>>> 47d8285 (rebase)
-=======
->>>>>>> ae00439 (rebase)
-        if (null == $this->panel) {
-=======
-<<<<<<< HEAD
-       
-=======
->>>>>>> 056bec8 (Lint)
-        if (null === $this->panel) {
->>>>>>> 883dd21 (up)
-=======
-        if (null == $this->panel) {
->>>>>>> ea58237 (up)
-=======
-    public function handle(): Renderable {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        if (null === $this->panel) {
->>>>>>> 27d1a3f (up)
-<<<<<<< HEAD
->>>>>>> 0088a11 (rebase)
-=======
-=======
-=======
-    public function handle(): Renderable
-    {
->>>>>>> 42ca46e (up)
-        if (null == $this->panel) {
->>>>>>> 1172c16 (up)
->>>>>>> 37f274f (rebase)
-=======
-=======
->>>>>>> d0b387a (rebase)
-=======
->>>>>>> 1fdd40b (rebase)
-        if (null == $this->panel) {
-=======
-       
-=======
->>>>>>> 056bec8 (Lint)
-        if (null === $this->panel) {
->>>>>>> 883dd21 (up)
-<<<<<<< HEAD
->>>>>>> f031549 (rebase)
-=======
-=======
-        if (null == $this->panel) {
->>>>>>> ea58237 (up)
->>>>>>> 1fdd40b (rebase)
-=======
-       
-=======
->>>>>>> 648e6d8 (rebase)
-=======
->>>>>>> d2bbae9 (rebase)
-        if (null === $this->panel) {
->>>>>>> 27d1a3f (up)
-<<<<<<< HEAD
->>>>>>> 0088a11 (rebase)
-<<<<<<< HEAD
->>>>>>> 355a234 (rebase)
-=======
-=======
-=======
-        if (null == $this->panel) {
->>>>>>> 1172c16 (up)
->>>>>>> 37f274f (rebase)
->>>>>>> d2bbae9 (rebase)
-=======
     public function handle(): Renderable {
         if (null == $this->panel) {
->>>>>>> 6d76482 (up)
             throw new \Exception('this->panel is null');
         }
 
