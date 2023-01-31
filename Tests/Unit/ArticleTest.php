9bd606ab5bdffb76b0baff2dcd14aa9ea78317db
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:34:35 +0100

    rebase

--- Tests/Unit/ArticleTest.php
+++ Tests/Unit/ArticleTest.php
@@ -16,15 +16,24 @@ class ArticleTest extends TestCase {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> d6d0ed6 (rebase)
+=======
+>>>>>>> a6f3582 (rebase)
 }
 =======
 }
 >>>>>>> da3c431 (up)
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 }
 >>>>>>> 78b6abe (Lint)
 =======
 >>>>>>> d6d0ed6 (rebase)
+=======
+=======
+}
+>>>>>>> 78b6abe (Lint)
+>>>>>>> a6f3582 (rebase)
