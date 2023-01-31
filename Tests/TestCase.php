2b05e1abb30a0ffc216088a35437059520269871
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:34:31 +0100

    rebase

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -9,6 +9,7 @@ namespace Modules\Blog\Tests;
 class TestCase extends \Orchestra\Testbench\TestCase {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -16,10 +17,15 @@ class TestCase extends \Orchestra\Testbench\TestCase
 =======
 class TestCase extends \Orchestra\Testbench\TestCase {
 >>>>>>> f0ffa9e (rebase)
+=======
+>>>>>>> d6d0ed6 (rebase)
 }
 =======
 }
 >>>>>>> da3c431 (up)
+<<<<<<< HEAD
 =======
 }
 >>>>>>> 78b6abe (Lint)
+=======
+>>>>>>> d6d0ed6 (rebase)
