2c04d8eb3f834b633ffdd604e222b8066956b109
marco76tv <marco.sottana@gmail.com>
2023-01-31 13:32:08 +0100

    .

--- Database/Factories/ContactFactory.php
+++ Database/Factories/ContactFactory.php
@@ -22,89 +22,24 @@ class ContactFactory extends Factory {
      */
     public function definition() {
         return [
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 648e6d8 (rebase)
-=======
->>>>>>> 0088a11 (rebase)
-=======
->>>>>>> 962cd73 (rebase)
-<<<<<<< HEAD
-=======
->>>>>>> f031549 (rebase)
-=======
->>>>>>> d0b387a (rebase)
-=======
-=======
->>>>>>> 0088a11 (rebase)
->>>>>>> 355a234 (rebase)
-=======
->>>>>>> 648e6d8 (rebase)
             // 'id' => $this->faker->randomNumber,
             // 'post_id' => $this->faker->integer,
             // 'post_type' => $this->faker->word,
-=======
-            //'id' => $this->faker->randomNumber,
-           // 'post_id' => $this->faker->integer,
-           // 'post_type' => $this->faker->word,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 355a234 (rebase)
->>>>>>> 883dd21 (up)
-=======
             // 'id' => $this->faker->randomNumber,
             // 'post_id' => $this->faker->integer,
             // 'post_type' => $this->faker->word,
->>>>>>> 056bec8 (Lint)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 27d1a3f (up)
-<<<<<<< HEAD
->>>>>>> 0088a11 (rebase)
-=======
-=======
             // 'id' => $this->faker->randomNumber,
             // 'post_id' => $this->faker->integer,
             // 'post_type' => $this->faker->word,
->>>>>>> 5a12252 (Lint)
->>>>>>> 962cd73 (rebase)
-=======
->>>>>>> 883dd21 (up)
->>>>>>> f031549 (rebase)
-=======
->>>>>>> d0b387a (rebase)
-=======
-=======
->>>>>>> 27d1a3f (up)
-<<<<<<< HEAD
->>>>>>> 0088a11 (rebase)
-<<<<<<< HEAD
->>>>>>> 355a234 (rebase)
-=======
-=======
-=======
             // 'id' => $this->faker->randomNumber,
             // 'post_id' => $this->faker->integer,
             // 'post_type' => $this->faker->word,
->>>>>>> 5a12252 (Lint)
->>>>>>> 962cd73 (rebase)
->>>>>>> 648e6d8 (rebase)
-=======
             // 'id' => $this->faker->randomNumber,
             // 'post_id' => $this->faker->integer,
             // 'post_type' => $this->faker->word,
->>>>>>> 6d76482 (up)
+            // 'id' => $this->faker->randomNumber,
+            // 'post_id' => $this->faker->integer,
+            // 'post_type' => $this->faker->word,
         ];
     }
 }
--- Database/Factories/PostFactory.php
+++ Database/Factories/PostFactory.php
@@ -1,110 +1,16 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 declare(strict_types=1);
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-declare(strict_types=1);
-
-=======
->>>>>>> 6c99a65 (.)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e7b73dd (rebase)
-=======
-declare(strict_types=1);
-
->>>>>>> 5c25f35 (Lint)
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-=======
-declare(strict_types=1);
-
->>>>>>> 6d76482 (up)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
 namespace Module\Blog\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Module\Blog\Tests\TestSupport\Models\Post;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-=======
->>>>>>> 6d76482 (up)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
 class PostFactory extends Factory {
     protected $model = Post::class;
 
     public function definition() {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
-class PostFactory extends Factory
-{
-    protected $model = Post::class;
-
-    public function definition()
-    {
->>>>>>> 6c99a65 (.)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e7b73dd (rebase)
-=======
-class PostFactory extends Factory {
-    protected $model = Post::class;
-
-    public function definition() {
->>>>>>> 5c25f35 (Lint)
-<<<<<<< HEAD
-=======
->>>>>>> f98d93c (rebase)
-=======
->>>>>>> e7b73dd (rebase)
-=======
->>>>>>> 6d76482 (up)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
         return [
             'title' => $this->faker->word,
         ];
