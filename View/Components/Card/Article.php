2ac89595b87668f9e61d61d1ef825e3783c75bc6
marco76tv <marco.sottana@gmail.com>
2023-01-31 10:39:44 +0100

    rebase

--- View/Components/Card/Article.php
+++ View/Components/Card/Article.php
@@ -18,6 +18,7 @@ class Article extends Component {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 class Article extends Component
 {
@@ -27,16 +28,24 @@ class Article extends Component {
 >>>>>>> f0ffa9e (rebase)
 =======
 >>>>>>> 03e54aa (rebase)
+=======
+>>>>>>> fd40c94 (rebase)
     public array $attrs = [];
 =======
     public array $attrs=[];
 >>>>>>> 071b412 (up)
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
     public array $attrs = [];
 >>>>>>> 7ae4511 (Lint)
 =======
 >>>>>>> 03e54aa (rebase)
+=======
+=======
+    public array $attrs = [];
+>>>>>>> 7ae4511 (Lint)
+>>>>>>> fd40c94 (rebase)
     public Model $row;
 
     /**
