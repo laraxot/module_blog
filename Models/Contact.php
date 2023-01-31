f261b6cc88ddb37f7cb5ff63546623d6d7dffc2c
marco76tv <marco76tv@users.noreply.github.com>
2023-01-31 10:20:05 +0000

    Lint

--- Models/Contact.php
+++ Models/Contact.php
@@ -6,32 +6,24 @@ namespace Modules\Blog\Models;
 
 /**
  * Modules\Blog\Models\Contact.
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 8213e79 (Lint)
-=======
-<<<<<<< HEAD
-=======
 >>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
+ * @property string|null                                                          $guid
+ * @property string|null                                                          $image_src
+ * @property string|null                                                          $lang
+ * @property string|null                                                          $post_type
+ * @property string|null                                                          $subtitle
+ * @property string|null                                                          $title
+ * @property string|null                                                          $txt
+ * @property string|null                                                          $user_handle
+ * @property \Modules\Lang\Models\Post|null                                       $post
+ * @property \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
+ * @property int|null                                                             $posts_count
+ * @property mixed                                                                $url
  *
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+ * @method static \Modules\Blog\Database\Factories\ContactFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Contact   newQuery()
 =======
-=======
->>>>>>> b4476b9 (Lint)
->>>>>>> 8213e79 (Lint)
-=======
->>>>>>> 2f1bc22 (rebase)
-=======
-=======
->>>>>>> 8213e79 (Lint)
->>>>>>> 6fa5a25 (rebase)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
  * @property string|null                                                          $guid
  * @property string|null                                                          $image_src
  * @property string|null                                                          $lang
@@ -65,43 +57,6 @@ namespace Modules\Blog\Models;
  * @method static \Modules\Blog\Database\Factories\ContactFactory     factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Contact       newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Contact       newQuery()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2f1bc22 (rebase)
-=======
->>>>>>> 6fa5a25 (rebase)
-=======
- * @property string|null $guid
- * @property string|null $image_src
- * @property-read string|null $lang
- * @property-read string|null $post_type
- * @property string|null $subtitle
- * @property string|null $title
- * @property string|null $txt
- * @property-read string|null $user_handle
- * @property-read \Modules\Lang\Models\Post|null $post
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
- * @property-read int|null $posts_count
- * @property-write mixed $url
- * @method static \Modules\Blog\Database\Factories\ContactFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Contact newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Contact newQuery()
->>>>>>> bc6d311 (up)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8213e79 (Lint)
-=======
->>>>>>> 2f1bc22 (rebase)
-=======
-=======
->>>>>>> 8213e79 (Lint)
->>>>>>> 6fa5a25 (rebase)
-=======
->>>>>>> b4476b9 (Lint)
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
  * @method static \Illuminate\Database\Eloquent\Builder|Contact       query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
--- Models/Home.php
+++ Models/Home.php
@@ -12,32 +12,33 @@ use Sushi\Sushi;
 
 /**
  * Modules\Blog\Models\Home.
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 8213e79 (Lint)
-=======
-<<<<<<< HEAD
-=======
 >>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
+ * @property int|null                                                              $id
+ * @property string|null                                                           $name
+ * @property string|null                                                           $icon_src
+ * @property string|null                                                           $created_by
+ * @property string|null                                                           $updated_by
+ * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Widget[] $containerWidgets
+ * @property int|null                                                              $container_widgets_count
+ * @property string|null                                                           $guid
+ * @property string|null                                                           $image_src
+ * @property string|null                                                           $lang
+ * @property string|null                                                           $post_type
+ * @property string|null                                                           $subtitle
+ * @property string|null                                                           $title
+ * @property string|null                                                           $txt
+ * @property string|null                                                           $user_handle
+ * @property \Modules\Lang\Models\Post|null                                        $post
+ * @property \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[]  $posts
+ * @property int|null                                                              $posts_count
+ * @property mixed                                                                 $url
+ * @property \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Widget[] $widgets
+ * @property int|null                                                              $widgets_count
  *
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+ * @method static \Modules\Blog\Database\Factories\HomeFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Home   newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Home   newQuery()
 =======
-=======
->>>>>>> b4476b9 (Lint)
->>>>>>> 8213e79 (Lint)
-=======
->>>>>>> 2f1bc22 (rebase)
-=======
-=======
->>>>>>> 8213e79 (Lint)
->>>>>>> 6fa5a25 (rebase)
-<<<<<<< HEAD
->>>>>>> 31b2874bced50f2cc56f4271d588e071d41fd17a
-=======
->>>>>>> 532d468a4759d4f5fa7956f2f94f26f0016c6484
  * @property int|null                                                              $id
  * @property string|null                                                           $name
  * @property string|null                                                           $icon_src
@@ -89,52 +90,6 @@ use Sushi\Sushi;
  * @method static \Modules\Blog\Database\Factories\HomeFactory        factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Home          newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Home          newQuery()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2f1bc22 (rebase)
-=======
->>>>>>> 6fa5a25 (rebase)
-=======
- * @property int|null $id
- * @property string|null $name
- * @property string|null $icon_src
- * @property string|null $created_by
- * @property string|null $updated_by
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Widget[] $containerWidgets
- * @property-read int|null $container_widgets_count
- * @property string|null $guid
- * @property string|null $image_src
- * @property-read string|null $lang
- * @property-read string|null $post_type
- * @property string|null $subtitle
- * @property string|null $title
- * @property string|null $txt
- * @property-read string|null $user_handle
- * @property-read \Modules\Lang\Models\Post|null $post
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
- * @property-read int|null $posts_count
- * @property-write mixed $url
- * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Xot\Models\Widget[] $widgets
- * @property-read int|null $widgets_count
- * @method static \Modules\Blog\Database\Factories\HomeFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Home newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Home newQuery()
->>>>>>> bc6d311 (up)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 8213e79 (Lint)
-=======
->>>>>>> 2f1bc22 (rebase)
-=======
-=======
->>>>>>> 8213e79 (Lint)
->>>>>>> 6fa5a25 (rebase)
-=======
->>>>>>> b4476b9 (Lint)
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
  * @method static \Illuminate\Database\Eloquent\Builder|Home          ofLayoutPosition($layout_position)
  * @method static \Illuminate\Database\Eloquent\Builder|Home          query()
