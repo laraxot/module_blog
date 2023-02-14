d294400e3a8383ed32dec5efd2322d8a0b2c2c07
marco xot <marco.sottana@gmail.com>
2023-02-14 12:58:48 +0100

    up

--- Models/Comment.php
+++ Models/Comment.php
@@ -9,56 +9,33 @@ use Modules\Rating\Models\Traits\RatingTrait;
 /**
  * Modules\Blog\Models\Comment.
  *
- * @property int                                                                      $id
- * @property string|null                                                              $post_type
- * @property int|null                                                                 $post_id
- * @property string|null                                                              $lang
- * @property int|null                                                                 $user_id
- * @property string|null                                                              $txt
- * @property string|null                                                              $created_by
- * @property string|null                                                              $updated_by
- * @property string|null                                                              $deleted_by
- * @property \Illuminate\Support\Carbon|null                                          $created_at
- * @property \Illuminate\Support\Carbon|null                                          $updated_at
- * @property int|null                                                                 $status
- * @property string|null                                                              $guid
- * @property string|null                                                              $image_src
- * @property \Illuminate\Support\Collection                                           $my_rating
- * @property float|null                                                               $ratings_avg
- * @property int|null                                                                 $ratings_count
- * @property string|null                                                              $subtitle
- * @property string|null                                                              $title
- * @property string|null                                                              $user_handle
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Rating\Models\Rating[] $myRatings
- * @property int|null                                                                 $my_ratings_count
- * @property \Modules\Lang\Models\Post|null                                           $post
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[]     $posts
- * @property int|null                                                                 $posts_count
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Rating\Models\Rating[] $ratingObjectives
- * @property int|null                                                                 $rating_objectives_count
- * @property \Illuminate\Database\Eloquent\Collection|\Modules\Rating\Models\Rating[] $ratings
- * @property mixed                                                                    $url
- *
- * @method static \Modules\Blog\Database\Factories\CommentFactory     factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       newQuery()
+ * @property string|null $guid
+ * @property string|null $image_src
+ * @property-read string|null $lang
+ * @property-read \Illuminate\Support\Collection $my_rating
+ * @property-read string|null $post_type
+ * @property-read float|null $ratings_avg
+ * @property-read int|null $ratings_count
+ * @property string|null $subtitle
+ * @property string|null $title
+ * @property string|null $txt
+ * @property-read string|null $user_handle
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Rating\Models\Rating[] $myRatings
+ * @property-read int|null $my_ratings_count
+ * @property-read \Modules\Lang\Models\Post|null $post
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Lang\Models\Post[] $posts
+ * @property-read int|null $posts_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Rating\Models\Rating[] $ratingObjectives
+ * @property-read int|null $rating_objectives_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\Rating\Models\Rating[] $ratings
+ * @property-write mixed $url
+ * @method static \Modules\Blog\Database\Factories\CommentFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang ofItem(string $guid)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       query()
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereCreatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereDeletedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereLang($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       wherePostId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       wherePostType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereStatus($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereTxt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereUpdatedBy($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModelLang withPost(string $guid)
- * @method static \Illuminate\Database\Eloquent\Builder|Comment       withRating()
- *
+ * @method static \Illuminate\Database\Eloquent\Builder|Comment withRating()
  * @mixin \Eloquent
  */
 class Comment extends BaseModelLang {
