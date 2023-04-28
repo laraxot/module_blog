44f5091760be122264c140606e4b7b91241229fd
marco76tv <marco.sottana@gmail.com>
2023-04-28 10:31:07 +0200

    up

--- Actions/GetCategoryOptionsByModelTypeAction.php
+++ Actions/GetCategoryOptionsByModelTypeAction.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 namespace Modules\Blog\Actions;
 
 use Modules\Blog\Models\Category;
+use Modules\Xot\Actions\GetModelClassByModelTypeAction;
 use Spatie\QueueableAction\QueueableAction;
 
 class GetCategoryOptionsByModelTypeAction
@@ -16,9 +17,20 @@ class GetCategoryOptionsByModelTypeAction
     /**
      * Execute the action.
      */
-    public function execute(string $model_type): array
+    public function execute(string $model_type, bool $only_filled = false): array
     {
-        $options = Category::ofType($model_type)->get()->pluck('name', 'id')->all();
+        $model_class = app(GetModelClassByModelTypeAction::class)->execute($model_type);
+
+        $options = Category::ofType($model_type)->get();
+
+        if ($only_filled) {
+            $options = $options
+                ->filter(function ($item) use ($model_class) {
+                    return $item->entries($model_class)->count() > 0;
+                });
+        }
+
+        $options = $options->pluck('name', 'id')->all();
 
         return $options;
     }
