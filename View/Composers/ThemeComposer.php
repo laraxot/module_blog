ea830def7a792e42de8e3aabfbefc6446bf0c212
marco76tv <marco76tv@users.noreply.github.com>
2023-05-09 08:04:07 +0000

    Lint

--- View/Composers/ThemeComposer.php
+++ View/Composers/ThemeComposer.php
@@ -11,13 +11,15 @@ use Modules\LU\Services\ProfileService;
 use Modules\Tag\Models\Tag;
 use Modules\Xot\Datas\XotData;
 
-class ThemeComposer {
+class ThemeComposer
+{
     /**
      * Undocumented function.
      *
      * @return Collection<Article>
      */
-    public function getFeaturedArticles(): Collection {
+    public function getFeaturedArticles(): Collection
+    {
         return Article::query()->whereHas('post')->limit(10)
             ->orderBy('created_at', 'desc')
             ->get();
@@ -39,7 +41,8 @@ class ThemeComposer {
      * ---.
      *  $latest_articles = $article->published()->publishedUntilToday()->orderBy('publish_date', 'desc')->take(3)->get();
      */
-    public function getLatestArticles(): Collection {
+    public function getLatestArticles(): Collection
+    {
         $rows = Article::published()
             ->publishedUntilToday()
             ->orderBy('published_at', 'desc')
@@ -53,7 +56,8 @@ class ThemeComposer {
      * ---.
      *  $authors = $user->userIsAuthor()->take(4)->get();
      */
-    public function getAuthors(): Collection {
+    public function getAuthors(): Collection
+    {
         return collect([]);
     }
 
@@ -61,19 +65,22 @@ class ThemeComposer {
      * ---.
      *  $navCategories = Category::has('articles', '>', '0')->take(8)->get();
      */
-    public function getNavCategories(): Collection {
+    public function getNavCategories(): Collection
+    {
         $res = Category::ofType('article')->get();
 
         return $res;
     }
 
-     public function getArticleCategories(): Collection {
+     public function getArticleCategories(): Collection
+     {
          $res = Category::ofType('article')->get();
 
          return $res;
      }
 
-     public function getArticleCategoriesOptions(): Collection {
+     public function getArticleCategoriesOptions(): Collection
+     {
          $options = $this->getArticleCategories()->pluck('name', 'id');
 
          return $options;
@@ -83,7 +90,8 @@ class ThemeComposer {
      * ----.
      * $footerAuthors = User::userIsAuthor()->take(8)->get();.
      */
-    public function getFooterAuthors(): Collection {
+    public function getFooterAuthors(): Collection
+    {
         $xot = XotData::make();
         // $profile_class = ProfileService::make()->getProfileClass();
         $profile_class = $xot->getProfileClass();
@@ -101,7 +109,8 @@ class ThemeComposer {
      * ----.
      *  $footerCategories = Category::has('articles', '>', '0')->take(8)->get();.
      */
-    public function getFooterCategories(): Collection {
+    public function getFooterCategories(): Collection
+    {
         return Category::ofType('article')->take(8)->get();
     }
 
@@ -131,7 +140,8 @@ class ThemeComposer {
      *        ->take(3)
      *        ->get();.
      */
-    public function getMoreArticles(Article $article): Collection {
+    public function getMoreArticles(Article $article): Collection
+    {
         $categories_ids = $article->categories->modelKeys();
         $rows = Article::published()
             ->publishedUntilToday()
@@ -147,7 +157,8 @@ class ThemeComposer {
      * ----.
      *   $articles = $model->with(['tags', 'category'])->orderBy('publish_date', 'desc')->paginate(10);.
      */
-    public function getPaginatedArticles(int $per_page): \Illuminate\Pagination\LengthAwarePaginator {
+    public function getPaginatedArticles(int $per_page): \Illuminate\Pagination\LengthAwarePaginator
+    {
         // Too few arguments to function Modules\Blog\Models\Article::scopeCategory(), 1 passed in
         $rows = Article::with(['tags' /* , 'category' */])
             ->orderBy('published_at', 'desc')
@@ -156,7 +167,8 @@ class ThemeComposer {
         return $rows;
     }
 
-    public function getPaginatedArticlesByAuthor(int $user_id): \Illuminate\Pagination\LengthAwarePaginator {
+    public function getPaginatedArticlesByAuthor(int $user_id): \Illuminate\Pagination\LengthAwarePaginator
+    {
         $rows = Article::published()
             ->publishedUntilToday()
             ->author($user_id)
@@ -170,7 +182,8 @@ class ThemeComposer {
      * Undocumented function
      *  $articles = $article->published()->publishedUntilToday()->tag($tag->id)->orderBy('publish_date', 'desc')->paginate(10);.
      */
-    public function getPaginatedArticlesByTag(string $tag): \Illuminate\Pagination\LengthAwarePaginator {
+    public function getPaginatedArticlesByTag(string $tag): \Illuminate\Pagination\LengthAwarePaginator
+    {
         $rows = Article::published()
             ->publishedUntilToday()
             ->withAnyTags([$tag])
@@ -188,7 +201,8 @@ class ThemeComposer {
      *       ->orderBy('publish_date', 'desc')
      *       ->paginate(10);.
      */
-    public function getPaginatedArticlesByCategoryId(int $id): \Illuminate\Pagination\LengthAwarePaginator {
+    public function getPaginatedArticlesByCategoryId(int $id): \Illuminate\Pagination\LengthAwarePaginator
+    {
         $rows = Article::published()
             ->publishedUntilToday()
             ->withCategories($id)
@@ -210,7 +224,8 @@ class ThemeComposer {
         // error 404
         // return 'aaa';
     // }
-    public function getCardEvents(): Collection {
+    public function getCardEvents(): Collection
+    {
         $str = '[
             {
               "date": "26",
@@ -284,7 +299,8 @@ class ThemeComposer {
         return collect(json_decode($str));
     }
 
-    public function getCardPlaces(): Collection {
+    public function getCardPlaces(): Collection
+    {
         $str = '[
             {
               "category": "Categoria",
